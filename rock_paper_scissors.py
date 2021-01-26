fd6f2a61a528f72f88bf883ebdbc8d774456457b
Lorenzo Laino <>
2021-01-26 23:27:17 +0100

    Create the model for rock_paper_scissors and saved on a json file, initialized the class for evaluate the model for rock paper and scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds 
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import cv2
 
 from tensorflow import keras
 
 # Process the data
-train_ds, test_ds = tfds.load('visual_domain_decathlon/gtsrb', split = ['train', 'test'], as_supervised = True)
+train_ds, test_ds = tfds.load('rock_paper_scissors', split = ['train', 'test'], as_supervised = True)
 
 train_ds = tfds.as_numpy(train_ds)
 test_ds = tfds.as_numpy(test_ds)
@@ -17,13 +17,13 @@ def create_img_label_set(data_set, dimension):
     label_set = []
     for img in data_set:
         image, label = img[0], img[1]
-        res = cv2.resize(image, dsize = (dimension, dimension), interpolation = cv2.INTER_CUBIC)
-        image_set.append(res)
+        res_img = cv2.resize(image, dsize = (dimension ,dimension), interpolation = cv2.INTER_CUBIC)
+        image_set.append(res_img)
         label_set.append(label)
     return np.asarray(image_set), np.asarray(label_set)
 
-img_train, label_train = create_img_label_set(test_ds, 72)
-img_test, label_test = create_img_label_set(test_ds, 72)
+img_train, label_train = create_img_label_set(train_ds, 30)
+img_test, label_test = create_img_label_set(test_ds, 30)
 print('Total images', img_train.shape[0] + img_test.shape[0])
 
 #plt.imshow(img_train[0])
@@ -34,6 +34,7 @@ print('Total images', img_train.shape[0] + img_test.shape[0])
 #print(img_train[5].shape)
 #print(img_train[2].shape)
 #print(img_train[29].shape)
+
 print('**', label_train)
 unique_label_values = np.unique(label_train)
 print('Labels number:', len(unique_label_values))
@@ -44,20 +45,21 @@ print('Test labels shape:', label_test.shape)
 
 # Build the model
 model = keras.Sequential()
-model.add(keras.layers.Conv2D(72, (3, 3), input_shape = (72, 72, 3)))
+model.add(keras.layers.Conv2D(64, (3, 3), input_shape = (30, 30, 3)))
 model.add(keras.layers.Activation('relu'))
 model.add(keras.layers.MaxPooling2D(pool_size = (2, 2)))
 
-model.add(keras.layers.Conv2D(36, (3, 3)))
+model.add(keras.layers.Conv2D(32, (3, 3)))
 model.add(keras.layers.Activation('relu'))
 model.add(keras.layers.MaxPooling2D(pool_size = (2, 2)))
 
-#model.add(keras.layers.Conv2D(64, (3, 3)))
-#model.add(keras.layers.Activation('relu'))
-#model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Conv2D(64, (3, 3)))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
 
 model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(72))
+#model.add(keras.layers.Dense(64))
+model.add(keras.layers.Dense(64)) #292*292 is the dimension after the 2 MaxPooling
 model.add(keras.layers.Activation('relu'))
 model.add(keras.layers.Dropout(0.5))
 model.add(keras.layers.Dense(len(unique_label_values), activation='softmax'))
@@ -67,11 +69,17 @@ model.add(keras.layers.Dense(len(unique_label_values), activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 # Train the model
-model.fit(img_train, label_train, validation_data = (img_test, label_test), epochs = 1, batch_size = 256)
+model.fit(img_train, label_train, validation_data = (img_test, label_test), epochs = 10, batch_size = 105)
 
 # Evaluate the model
 accuracy = model.evaluate(img_test, label_test, verbose = 0)
+print(model.metrics_names)
 print('Accuracy: %.2f%%' % (accuracy[1]*100))
 
-# Make predictions
+# Save the model
+json_file = model.to_json()
+with open('/Users/lorenzolaino/llDev/intelligent_systems/prova.json', 'w') as file:
+    file.write(json_file)
 
+# Serialize weights to HDF5
+model.save_weights('/Users/lorenzolaino/llDev/intelligent_systems/prova_weights.hdf5')
