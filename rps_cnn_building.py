1ae094a8ccace98f2bf28df19375b8708e639515
Marco <mgatto1@studenti.uninsubria.it>
2021-02-07 18:53:28 +0100

    Trained models added, cnn building and evaluation refactored

--- rps_cnn_building.py
+++ rps_cnn_building.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from tensorflow import keras
 
 from rps_process_data import RPSProcessData
@@ -9,20 +11,20 @@ RPS_process_data.print_dataset_shapes()
 
 # Build the model
 model = keras.Sequential()
-model.add(keras.layers.Conv2D(64, (3, 3), input_shape=(50, 50, 3)))
+model.add(keras.layers.Conv2D(50, (3, 3), input_shape=(50, 50, 3)))
 model.add(keras.layers.Activation('relu'))
 model.add(keras.layers.MaxPooling2D(pool_size=(3, 3)))
 
-model.add(keras.layers.Conv2D(32, (3, 3)))
+model.add(keras.layers.Conv2D(25, (3, 3)))
 model.add(keras.layers.Activation('relu'))
 model.add(keras.layers.MaxPooling2D(pool_size=(3, 3)))
 
-model.add(keras.layers.Conv2D(64, (3, 3)))
+model.add(keras.layers.Conv2D(50, (3, 3)))
 model.add(keras.layers.Activation('relu'))
 model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
 
 model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(64))
+model.add(keras.layers.Dense(50))
 model.add(keras.layers.Activation('relu'))
 model.add(keras.layers.Dropout(0.5))
 model.add(keras.layers.Dense(len(label_names), activation='softmax'))
@@ -30,20 +32,33 @@ model.add(keras.layers.Dense(len(label_names), activation='softmax'))
 # Compile the model
 model.compile(loss='sparse_categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
+
+def plot_hist(hist):
+    plt.plot(hist.history["accuracy"])
+    plt.plot(hist.history["val_accuracy"])
+    plt.title("model accuracy")
+    plt.ylabel("accuracy")
+    plt.xlabel("epoch")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.show()
+
+
 # Train the model
-model.fit(img_train, label_train, validation_data=(img_test, label_test), epochs=10, batch_size=105)
+hist = model.fit(img_train, label_train, validation_data=(img_test, label_test), epochs=10, batch_size=105)
+plot_hist(hist)
 
 # Evaluate the model
 accuracy = model.evaluate(img_test, label_test, verbose=0)
 print(model.metrics_names)
 print('Accuracy: %.2f%%' % (accuracy[1] * 100))
 
+proj_dir_path = 'D:\\Utenti\\Marco\\Desktop\\Insubria\\Laurea Magistrale\\Intelligent Systems\\Exam Project Gatto ' \
+                'Laino\\intelligent_systems'
+
 # Save the model
-json_path_file = 'D:\\Utenti\\Marco\\Desktop\\Insubria\\Laurea Magistrale\\Intelligent Systems\\Exam Project Gatto Laino\\intelligent_systems\\rps_50_cnn_model.json'
-hdf5_path_file = 'D:\\Utenti\\Marco\\Desktop\\Insubria\\Laurea Magistrale\\Intelligent Systems\\Exam Project Gatto Laino\\intelligent_systems\\rps_50_cnn_model_weights.hdf5'
 json_file = model.to_json()
-with open(json_path_file, 'w') as file:
+with open(f'{proj_dir_path}\\trained_models\\rps_10_epochs_cnn_model.json', 'w') as file:
     file.write(json_file)
 
 # Serialize weights to HDF5
-model.save_weights(hdf5_path_file)
+model.save_weights(f'{proj_dir_path}\\trained_models\\rps_10_epochs_cnn_model_weights.hdf5')
--- rps_cnn_evaluation.py
+++ rps_cnn_evaluation.py
@@ -1,5 +1,7 @@
+import itertools
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import confusion_matrix
 from tensorflow import keras
 
 from rps_process_data import RPSProcessData
@@ -8,18 +10,53 @@ RPS_process_data = RPSProcessData()
 img_train, label_train, img_test, label_test = RPS_process_data.train_test_split()
 label_names = RPS_process_data.label_names
 
+proj_dir_path = 'D:\\Utenti\\Marco\\Desktop\\Insubria\\Laurea Magistrale\\Intelligent Systems\\Exam Project Gatto ' \
+                'Laino\\intelligent_systems'
+
 # Load json and create model
-json_path_file = 'D:\\Utenti\\Marco\\Desktop\\Insubria\\Laurea Magistrale\\Intelligent Systems\\Exam Project Gatto Laino\\intelligent_systems\\rps_50_cnn_model.json'
-hdf5_path_file = 'D:\\Utenti\\Marco\\Desktop\\Insubria\\Laurea Magistrale\\Intelligent Systems\\Exam Project Gatto Laino\\intelligent_systems\\rps_50_cnn_model_weights.hdf5'
-file = open(json_path_file, 'r')
+file = open(f'{proj_dir_path}\\trained_models\\rps_10_epochs_cnn_model.json', 'r')
 json_model = file.read()
 file.close()
-
 loaded_model = keras.models.model_from_json(json_model)
+
 # Load weights
-loaded_model.load_weights(hdf5_path_file)
+loaded_model.load_weights(f'{proj_dir_path}\\trained_models\\rps_10_epochs_cnn_model_weights.hdf5')
 
 predictions = loaded_model.predict(img_test)
+
+
+def plot_confusion_matrix(cm):
+    plt.figure(figsize=(8, 8))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title("Confusion matrix")
+    plt.colorbar()
+    tick_marks = np.arange(len(label_names))
+    plt.xticks(tick_marks, label_names, rotation=45)
+    plt.yticks(tick_marks, label_names)
+
+    # Normalize the confusion matrix.
+    conf_matrix = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+
+    # Use white text if squares are dark; otherwise black.
+    threshold = conf_matrix.max() / 2.
+
+    for i, j in itertools.product(range(conf_matrix.shape[0]), range(conf_matrix.shape[1])):
+        color = "white" if conf_matrix[i, j] > threshold else "black"
+        plt.text(j, i, conf_matrix[i, j], horizontalalignment="center", color=color)
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+
+# Use the model to predict the values from the test_images.
+test_pred = np.argmax(predictions, axis=1)
+
+# Calculate the confusion matrix using sklearn.metrics
+cm = confusion_matrix(label_test, test_pred)
+
+plot_confusion_matrix(cm)
 
 
 def plot_image(i):
